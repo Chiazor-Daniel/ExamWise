6ba77922155028521d7d23f1ce183e31a1f3ee4d
Chiazor Daniel <chiazordaniel317@gmail.com>
2025-07-14 08:45:24 +0000

    fx

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -35,7 +35,7 @@ const SidebarContent = () => {
                 <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                     <Link href="/" className="flex items-center gap-2 font-semibold">
                         <Lightbulb className="h-6 w-6 text-primary" />
-                        <span className="font-headline text-xl">ExamWise</span>
+                        {/* <span className="font-headline text-xl">ExamWise</span> */}
                     </Link>
                 </div>
                 <div className="flex-1">
